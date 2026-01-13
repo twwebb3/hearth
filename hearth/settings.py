9fe6b08907855dbbed2a03e7ecf70145de1ed18b
TW Webb <twwebb1@me.com>
2026-01-13 14:08:25 -0500

    login added

--- hearth/settings.py
+++ hearth/settings.py
@@ -126,3 +126,7 @@ STATIC_URL = "static/"
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
+# Authentication
+LOGIN_REDIRECT_URL = "dashboard"
+LOGOUT_REDIRECT_URL = "dashboard"
